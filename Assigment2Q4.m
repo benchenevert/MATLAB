61312ae55969507a151d268e40532e722e1181e6
benchenevert <ben.chenevert@wsu.edu>
2022-07-13 23:35:38 -0700

    Tried one last time HW 2 Q4

--- Assigment2Q4.m
+++ Assigment2Q4.m
@@ -1,3 +1,28 @@
+%% Question 4
+
+%%Variables
+s = sum(r,"all")
+r = rand
+
+while (sum(r)<20)
+       r = rand
+    if (sum(r)>20)
+
+        s = sum(r,'all')
+    end
+end
+disp(s);
 
 
-    
+%j = sum(r,'all')
+
+%for (j <= 20)
+
+       % r = rand
+        %if (j > 20)
+         %   then 
+          %  break
+       % end
+%end
+
+%% Sorry I just don't know how to complete this problem
