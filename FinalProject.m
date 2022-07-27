69829d8d962afbae1968722c18a9e18ec269ad2e
benchenevert <ben.chenevert@wsu.edu>
2022-07-26 22:45:42 -0700

    Day 1
    Struggled heavily with variable location and creating correct for loop

---
+++ FinalProject.m
@@ -0,0 +1,29 @@
+%% Variables
+
+x = 0;
+h = (0);
+a = 0;
+%% How can i set a to a place holder until a value is input
+a = x + h
+b = 0;
+%% Attempt 1
+%%function slope = (f(x+h)-f(x))/h
+
+function f = input()
+function g = input()
+
+    %for h = ("value")
+    %    a = x + h;
+
+     %   while f = ("value")
+            
+
+        
+
+jumble = (g - f) / h;
+
+print(jumble);
+end
+end
+
+%end
